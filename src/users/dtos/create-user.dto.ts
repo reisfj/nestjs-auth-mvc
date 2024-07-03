01c4a4555b460b55ee999a97667070a7d24ee0db
reis.fj <reis.fjr@gmail.com>
2024-07-02 23:26:20 -0300

    feat(users): add user-related DTOs
    Add DTOs for creating and logging in users.
    - create-user.dto.ts
    - login-user.dto.ts

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,43 @@
+import { IsNotEmpty, IsEmail, IsString, Matches, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    @IsString()
+    fullName: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @MinLength(6)
+    password: string;
+
+    @IsNotEmpty()
+    @MinLength(6)
+    confirmPassword: string;
+
+    @IsNotEmpty()
+    @IsString()
+    street: string;
+
+    @IsNotEmpty()
+    @IsString()
+    neighborhood: string;
+
+    @IsNotEmpty()
+    @IsString()
+    number: string;
+
+    @IsNotEmpty()
+    @IsString()
+    city: string;
+
+    @IsNotEmpty()
+    @IsString()
+    state: string;
+
+    @IsNotEmpty()
+    @Matches(/^[0-9]{5}-[0-9]{3}$/)
+    cep: string;
+}
