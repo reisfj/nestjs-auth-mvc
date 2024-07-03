234878d0ed2bcec1b85eca680114019a6369bbd1
reis.fj <reis.fjr@gmail.com>
2024-07-02 23:32:12 -0300

    fix(users): adjust import of create-user-dto in users.service.ts
    Adjust the import statement for create-user-dto to ensure proper functionality.
    - users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './schemas/user.schema';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto } from '../users/dtos/create-user.dto';
 
 @Injectable()
 export class UsersService {
